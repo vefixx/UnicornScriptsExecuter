44d931356d382ca651871733db26a946c312f506
vefixx <vef1xx@yandex.ru>
2026-05-05 19:43:28 +0500

    .gitignore, убрано чтение ts последней строки таблицы, адаптация под INSERT OR REPLACE

--- UnicornStatsUpdater/Updaters/HourlyUpdater.cs
+++ UnicornStatsUpdater/Updaters/HourlyUpdater.cs
@@ -22,18 +22,25 @@ public class HourlyUpdater : IUpdater
         await using (var con = new SqliteConnection(connectionString))
         {
             await con.OpenAsync();
-            var lastUpdateString = await con.QuerySingleOrDefaultAsync<string>("SELECT ts FROM m_stats_hourly_by_apartment ORDER BY ts DESC LIMIT 1");
 
-            DateTime lastUpdate = lastUpdateString != null ? DateTime.Parse(lastUpdateString) : new DateTime(1900, 1, 1);
-            Log.Information($"Последнее обновление таблицы: {lastUpdateString}");
-
-            var parameters = new { lastHourlyUpdate = lastUpdate };
-            
-            foreach (var sqlFile in files)
+            await using (var transaction = await con.BeginTransactionAsync())
             {
-                Log.Information($"Выполнение скрипта \"{sqlFile}\"");
-                var sql = await File.ReadAllTextAsync(sqlFile);
-                await con.ExecuteAsync(sql, parameters);
+                try
+                {
+                    foreach (var sqlFile in files)
+                    {
+                        Log.Information($"Выполнение скрипта \"{sqlFile}\"");
+                        var sql = await File.ReadAllTextAsync(sqlFile);
+                        await con.ExecuteAsync(sql);
+                    }
+
+                    await transaction.CommitAsync();
+                }
+                catch (Exception e)
+                {
+                    Log.Error($"Ошибка при выполнении одного из скрипта: {e.Message}");
+                    await transaction.RollbackAsync();
+                }
             }
         }
         
